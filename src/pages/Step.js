39e66a1dd2ff2adf35767a4ab179d804b286083a
musyahya <mananabdul37654@gmail.com>
2021-08-16 09:04:22 +0700

    14 menampilkan step dari todolist id

--- src/pages/Step.js
+++ src/pages/Step.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { withRouter } from "react-router-dom";
+import { Api_Url } from '../utility/Api_Url';
 
-const Step = () => {
+const Step = (props) => {
+
+    const [step, setStep] = useState("");
+
+     useEffect(() => {
+       getByTodolistId();
+     }, []);
+
+     function getByTodolistId(){
+           axios
+             .get(
+               Api_Url + "step/" + props.match.params.id + "/getByTodolistId"
+             )
+             .then(function (response) {
+               setStep(response.data);
+               // console.log(response);
+             })
+             .catch(function (error) {
+               // console.log(error);
+             });
+     }
+
+    console.log(step);
+
     return (
         <div>
             <h1>Halaman step</h1>
@@ -8,4 +34,4 @@ const Step = () => {
     );
 };
 
-export default Step;
+export default withRouter(Step);
